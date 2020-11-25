a2185794627ca7dd9aa7c4e3778699503a0b85a0
wyattschwanbeck <wyattschwanbeck@gmail.com>
2020-11-25 16:32:08 -0500

    Update ShotsTracking.py
    Removed unused code and included update for dynamic resolution.

--- ShotsTracking.py
+++ ShotsTracking.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
 """
 The goal of this program is to record shots on War of Rights Shooting range to
-assist with accuracy and consistancy in shots.
-Currently the code is not dynamic and was written to capture footage via
-1920X1080 resolution on a second monitor.
-Changing values for monitor variable on the functions Capture_Sights and Capture_Results
-will allow you to adjust for screen resolution.
-Changing the size of the captured images for either of these may require you to adjust
-the paste call on RingBuffers method CreateGif. It is set to paste the lower
+assist with accuracy and consistancy in shots and even record 'shot cams' for matches
+Currently the code is dynamic and is written to capture any resolution
+    in which the game is played as long as it is fullscreen+borderless
+
 """
 
 import mss
@@ -16,7 +12,14 @@ import time
 import datetime
 import os
 
+
+
+
 from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+
 from collections import deque
 
 class RingBuffer(deque):
@@ -43,95 +46,137 @@ class RingBuffer(deque):
 		"""returns a list of size items (newest items)"""
 		return list(self)
 
-	def CreateGif(self, current_date, shot_taken, final_result):
+	def CreateGif(self, fireSeconds, final_result):
 		aim_frames = self.get()
-		final_result = Image.frombytes("RGB",
-								 final_result.size,
-								 final_result.bgra, "raw", "BGRX")
+		final_result=Image.fromarray(final_result.astype('uint8'), 'RGB')
 		final_frames = []
-		print(len(aim_frames))
+
+		font = ImageFont.truetype("font/HeadlinerNo45-59y8.ttf", 20)
+
+
 		for f,frame in enumerate(aim_frames,0):
-			frame = Image.frombytes("RGB",
-						   frame.size,
-						   frame.bgra, "raw", "BGRX")
+			frame =Image.fromarray(frame.astype('uint8'), 'RGBA')
 			final_result_copy = final_result.copy()
 			final_result_copy.paste(frame,
 						   (final_result_copy.size[0]-frame.size[0],
 							  final_result_copy.size[1]-frame.size[1]))
+
+			draw = ImageDraw.Draw(final_result_copy)
+			draw.rectangle(((0, 00), (final_result.size[0],15)), fill="black")
+			draw.text((0, 2),"Seconds from aiming to firing:{0}".format(fireSeconds),(255,255,255),font=font)
 			final_frames.append(final_result_copy)
-			final_frames[0].save(
-				"{0}/{1}.gif".format(current_date, shot_taken),
-				format="GIF", append_images=final_frames[1:],
-				save_all=True, loop=0, fps =30, optimize=True)
+		final_frames[0].save(
+			"results/{0}.gif".format(datetime.datetime.now().strftime("%d-%m-%Y %H%M-%S")),
+			format="GIF", append_images=final_frames[1:],
+			save_all=True, loop=0, fps =30)
 
 
-def Capture_Result(current_date, shot_taken):
+def Capture_Result(monitorNum):
 	'''Captures the shooting range result'''
 	time.sleep(1)
 	with mss.mss() as sct:
-		monitor = {"top":210, "left":850, "width":475, "height":225,"mon":1}
-		sct_img = sct.grab(monitor)
-		return sct_img
+		image = np.array(sct.grab(sct.monitors[monitorNum]))
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	return adjusted_capture(image,810,150,350,525)
 
 
-def Capture_Sights(current_date, shot_taken, ss_num):
+def Capture_Sights(monitorNum):
 	'''Capture a the last 100 frames since shooting'''
 	with mss.mss() as sct:
-		monitor = {"top":480, "left":920, "width":80, "height":80,"mon":1}
-		if os.path.exists(current_date)==False:
-			os.mkdir(current_date)
-		if os.path.exists("{0}/{1}".format(current_date,shot_taken)) == False:
-			os.mkdir("{0}/{1}".format(current_date,shot_taken))
 
-		sct_img = sct.grab(monitor)
-		return sct_img
+		if os.path.exists("results")==False:
+			os.mkdir("results")
 
-def Start_Count_Down_Then_Capture(current_date, shot_taken):
+		image = np.array(sct.grab(sct.monitors[monitorNum]))
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
+		return adjusted_capture(image,910,470,100,100)
+
+
+
+def Start_Count_Down_Then_Capture(monitorNum):
 	ring = RingBuffer(100)
 
-	print("Count Down Initiated")
+	print("User Aiming")
 	start_time = time.time()
 	state_left = win32api.GetKeyState(0x01)
 
 	state_left_time = time.time()
 	state_right = win32api.GetKeyState(0x02)
 
-	while time.time()-start_time<4:
+	while time.time()-start_time<2:
 
 		if state_right ==0:
 			return False
 		else:
 			state_right = win32api.GetKeyState(0x02)
+
 	capture_count = 0
 	while state_right < 0:
-		ring.append(Capture_Sights(current_date,shot_taken,capture_count))
+		ring.append(Capture_Sights(monitorNum))
 		state_right = win32api.GetKeyState(0x02)
 		state_left = win32api.GetKeyState(0x01)
 		capture_count+=1
 
 		if state_left < 0:
+			print("Shot taken, rendering gif")
 			state_left_time = time.time()
 			while time.time() - state_left_time < .25:
-				ring.append(Capture_Sights(current_date,shot_taken,capture_count))
+				ring.append(Capture_Sights(monitorNum))
 
-			final_result = Capture_Result(current_date,shot_taken)
-			ring.CreateGif(current_date, shot_taken, final_result)
+			fireSeconds = round(time.time()-start_time,2)
+			final_result = Capture_Result(monitorNum)
+
+			ring.CreateGif(fireSeconds, final_result)
+			print("Gif rendered, ready to capture next shot")
 			return True
 			break
 
 	return False
 
+import numpy as np
 
+import cv2
+
+def adjusted_capture(screen, base_pixel_x, base_pixel_y, y_size, x_size):
+        screen_w_adj = (screen.shape[0]/1080)
+        screen_h_adj = (screen.shape[1]/1920)
+
+        aspect_ratio = screen.shape[0]/screen.shape[1]
+        aspect_adj = (1080/1920)/aspect_ratio
+
+        screen = np.array(screen[int((base_pixel_y*(1/aspect_adj))*screen_h_adj):\
+									 int((y_size*(screen.shape[0]/1080) \
+									  +((base_pixel_y*(1/aspect_adj))*screen_h_adj))),
+                                int((base_pixel_x*aspect_adj)*screen_w_adj):\
+									int((x_size*(screen.shape[1]/1920) \
+							     +(base_pixel_x*aspect_adj)*screen_w_adj)), :])
+
+        return screen
+print("Shot Tracker for War of Rights Shooting Range")
+
+monitorNum = input("Enter Monitor Number:")
+while monitorNum.isalnum()==False:
+	monitorNum = input("Invalid input, Enter Monitor Number:")
+
+monitorNum = int(monitorNum)
 
 shot_taken = 0
+
+print("Monitoring for aim...")
 while True:
+
 	current_date = datetime.date.today().strftime("%d-%m-%Y")
 	state_right = win32api.GetKeyState(0x02)
 
 	if state_right < 0:
 
-		if Start_Count_Down_Then_Capture(current_date,shot_taken):
+		if Start_Count_Down_Then_Capture(monitorNum):
 			shot_taken += 1
+			print("{0} Shot(s) Logged".format(shot_taken))
+
+		else:
+			print("Shot Aborted")
+
 
 
 
